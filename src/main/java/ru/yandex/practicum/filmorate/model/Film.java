4137d09460da880dcdbf9dbf192d137f9fe89daf
User Aitalina <aitautau@gmail.com>
2025-11-16 17:27:14 +0500

    Спринт 10 Проект Filmorate

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,27 +1,28 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
 public class Film {
-    @NotNull(message = "ID фильма обязательно")
     private Integer id;
 
+    @NotNull(message = "Названиее фильма обязательно")
     @NotBlank(message = "Название фильма не может быть пустым")
     private String name;
 
+    @NotNull(message = "Описание фильма обязательно")
     @NotBlank(message = "Описание фильма не может быть пустым")
+    @Size(max = 200, message = "Описание фильма не может превышать 200 символов")
     private String description;
 
+    @NotNull(message = "Дата релиза обязательна")
     @PastOrPresent(message = "Дата релиза не может быть в будущем")
     private LocalDate releaseDate;
 
+    @NotNull(message = "Продолжительность фильма обязательна")
     @Positive(message = "Продолжительность должна быть положительной")
     private Integer duration;
 }
