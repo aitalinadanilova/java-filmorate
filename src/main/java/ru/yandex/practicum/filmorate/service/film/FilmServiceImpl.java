c97ddfc47c5fede68782de8b562b77c27ce61f02
User Aitalina <aitautau@gmail.com>
2025-12-11 19:03:37 +0500

    Спринт 11 Проект Filmorate

--- src/main/java/ru/yandex/practicum/filmorate/service/film/FilmServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/FilmServiceImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class FilmServiceImpl implements FilmService{
+public class FilmServiceImpl implements FilmService {
 
     private final FilmStorage filmStorage;
 
