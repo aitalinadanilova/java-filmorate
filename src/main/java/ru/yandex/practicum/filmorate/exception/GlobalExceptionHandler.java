85be522d3226c308ab9e80be5ba9b64945c5b094
User Aitalina <aitautau@gmail.com>
2025-12-12 16:28:16 +0500

    Спринт 11 Проект Filmorate

--- src/main/java/ru/yandex/practicum/filmorate/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/GlobalExceptionHandler.java
@@ -1,41 +1,49 @@
 package ru.yandex.practicum.filmorate.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleNotFound(NotFoundException e) {
+    public ResponseEntity<ErrorResponse> handleNotFound(final NotFoundException e) {
+        log.warn("404 {}", e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(ValidationException.class)
-    public ResponseEntity<ErrorResponse> handleValidation(ValidationException e) {
+    public ResponseEntity<ErrorResponse> handleValidation(final ValidationException e) {
+        log.warn("400 {}", e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new ErrorResponse(e.getMessage()));
     }
 
-    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
-
-        String errorMessage = e.getBindingResult().getFieldErrors().stream()
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(final MethodArgumentNotValidException e) {
+        String message = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
                 .findFirst()
-                .map(err -> err.getField() + ": " + err.getDefaultMessage())
+                .map(field -> field.getDefaultMessage())
                 .orElse("Ошибка валидации");
 
+        log.warn("400 {}", message);
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(new ErrorResponse(errorMessage));
+                .body(new ErrorResponse(message));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleOther(Exception e) {
+    public ResponseEntity<ErrorResponse> handleOther(final Exception e) {
+        log.error("500 {}", e.getMessage(), e);
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ErrorResponse("Внутренняя ошибка сервера: " + e.getMessage()));
