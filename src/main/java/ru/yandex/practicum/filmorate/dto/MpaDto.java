980ca20baf44c6d6394a76317885b641fa92eee6
User Aitalina <aitautau@gmail.com>
2025-12-27 16:14:18 +0500

    Спринт 12 Проект Filmorate

--- src/main/java/ru/yandex/practicum/filmorate/dto/MpaDto.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/MpaDto.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.filmorate.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
