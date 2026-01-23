9d58574f2c753b6e83e0726e2ec4a177a728f0b7
User Aitalina <aitautau@gmail.com>
2026-01-23 18:48:46 +0500

    Исправление теста

--- src/test/java/ru/yandex/practicum/filmorate/storage/review/ReviewDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/review/ReviewDbStorageTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import ru.yandex.practicum.filmorate.model.Review;
