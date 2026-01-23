3ddd9fd088041fe604afc4039f20b671560a10db
Kuznetsov Maksim <makkownis@yandex.ru>
2026-01-23 22:40:30 +1000

    Правки

--- src/main/java/ru/yandex/practicum/filmorate/service/feed/FeedServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/feed/FeedServiceImpl.java
@@ -23,13 +23,7 @@ public class FeedServiceImpl implements FeedService {
     @Override
     public void createFeed(Long userId, EventType eventType, Operation operation, Long entityId) {
         log.info("Создание объекта feed: userId {}, eventType {}, operation {}, entityId {}", userId, eventType, operation, entityId);
-        Feed feed = Feed.builder()
-                .timestamp(System.currentTimeMillis())
-                .userId(userId)
-                .eventType(eventType)
-                .operation(operation)
-                .entityId(entityId)
-                .build();
+        Feed feed = Feed.builder().timestamp(System.currentTimeMillis()).userId(userId).eventType(eventType).operation(operation).entityId(entityId).build();
 
         feedStorage.createFeed(feed);
     }
